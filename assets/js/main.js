63a3c0c22ab6a7243bc0298703309172317a30ce
Konark-Verma <konarkverma.dev@gmail.com>
2026-03-28 17:11:40 +0530

    Responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,22 @@
 /*=============== SHOW MENU ===============*/
-
+const navMenu = document.getElementById('nav-menu'),
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close');
 /*===== Menu Show =====*/
 /* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click',()=>{
+        navMenu.classList.add('show-menu');
+    });
+}
 
 /*===== Hide Show =====*/
 /* Validate if constant exists */
-
+if(navClose){
+    navClose.addEventListener('click',()=>{
+        navMenu.classList.remove('show-menu');
+    });
+}
 /*=============== IMAGE GALLERY ===============*/
 function imgGallery(){
     const mainImg = document.querySelector('.details__img'),
@@ -28,13 +39,21 @@ var swiperCategories = new Swiper('.categories__container', {
     },
 
     breakpoints: {
-        640: {
+        350: {
             slidesPerView: 2,
-            spaceBetween: 20,
+            spaceBetween: 24,
         },
         768: {
+            slidesPerView: 3,
+            spaceBetween: 24,
+        },
+        992: {
             slidesPerView: 4,
-            spaceBetween: 40,
+            spaceBetween: 24,
+        },
+        1200: {
+            slidesPerView: 5,
+            spaceBetween: 24,
         },
         1400: {
             slidesPerView: 6,
@@ -53,13 +72,13 @@ var swiperProducts = new Swiper('.new__container', {
     },
 
     breakpoints: {
-        640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
         768: {
-            slidesPerView: 4,
-            spaceBetween: 40,
+            slidesPerView: 2,
+            spaceBetween: 24,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 24,
         },
         1400: {
             slidesPerView: 4,
